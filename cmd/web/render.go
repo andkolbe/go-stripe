474cbfc8dba44fd178cfd098b678a59b329f0007
Andrew <kolbe1129@gmail.com>
2021-08-31 22:13:33 -0500

    added template for the home page

--- cmd/web/render.go
+++ cmd/web/render.go
@@ -8,13 +8,12 @@ import (
 	"strings"
 )
 
-// all of the data we might need to pass into a template
 type templateData struct {
 	StringMap       map[string]string
 	IntMap          map[string]int
 	FloatMap        map[string]float32
 	Data            map[string]interface{}
-	CSRFTpken       string
+	CSRFToken       string
 	Flash           string
 	Warning         string
 	Error           string
@@ -23,14 +22,12 @@ type templateData struct {
 	CSSVersion      string
 }
 
-var functions = template.FuncMap {
+var functions = template.FuncMap{}
 
-}
-
-// go:embed templates
+//go:embed templates
 var templateFS embed.FS
 
-// where we will add any info in the templateData struct or any variable that is create with a type of template data
+
 func (app *application) addDefaultData(td *templateData, r *http.Request) *templateData {
 	return td
 }
@@ -38,14 +35,12 @@ func (app *application) addDefaultData(td *templateData, r *http.Request) *templ
 func (app *application) renderTemplate(w http.ResponseWriter, r *http.Request, page string, td *templateData, partials ...string) error {
 	var t *template.Template
 	var err error
-	templateToRender := fmt.Sprintf("templates/%s.page.tmpl", page)
+	templateToRender := fmt.Sprintf("templates/%s.page.gohtml", page)
 
 	_, templateInMap := app.templateCache[templateToRender]
 
-	// if we are in production, check if the template is in the cache. If it is, use it
 	if app.config.env == "production" && templateInMap {
 		t = app.templateCache[templateToRender]
-	// else build it
 	} else {
 		t, err = app.parseTemplate(partials, page, templateToRender)
 		if err != nil {
@@ -54,15 +49,12 @@ func (app *application) renderTemplate(w http.ResponseWriter, r *http.Request, p
 		}
 	}
 
-	// check to see if data was passed with a call to addDefaultData
-	// if it wasn't we create an empty template data object
 	if td == nil {
 		td = &templateData{}
 	}
-	// then add the default data
+
 	td = app.addDefaultData(td, r)
 
-	// execute template
 	err = t.Execute(w, td)
 	if err != nil {
 		app.errorLog.Println(err)
@@ -75,18 +67,18 @@ func (app *application) renderTemplate(w http.ResponseWriter, r *http.Request, p
 func (app *application) parseTemplate(partials []string, page, templateToRender string) (*template.Template, error) {
 	var t *template.Template
 	var err error
-
+	
 	// build partials
 	if len(partials) > 0 {
 		for i, x := range partials {
-			partials[i] = fmt.Sprintf("templates/%s.partial.tmpl", x)
+			partials[i] = fmt.Sprintf("templates/%s.partial.gohtml", x)
 		}
 	}
 
 	if len(partials) > 0 {
-		t, err = template.New(fmt.Sprintf("%s.page.tmpl", page)).Funcs(functions).ParseFS(templateFS, "templates/base.layout.tmpl", strings.Join(partials, ","), templateToRender)
+		t, err = template.New(fmt.Sprintf("%s.page.gohtml", page)).Funcs(functions).ParseFS(templateFS, "templates/base.layout.gohtml", strings.Join(partials, ","), templateToRender)
 	} else {
-		t, err = template.New(fmt.Sprintf("%s.page.tmpl", page)).Funcs(functions).ParseFS(templateFS, "templates/base.layout.tmpl", templateToRender)
+		t, err = template.New(fmt.Sprintf("%s.page.gohtml", page)).Funcs(functions).ParseFS(templateFS, "templates/base.layout.gohtml", templateToRender)
 	}
 	if err != nil {
 		app.errorLog.Println(err)
