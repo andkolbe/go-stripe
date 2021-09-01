70eb547da8fe831a47162d473d41369514569115
Andrew <kolbe1129@gmail.com>
2021-09-01 14:14:53 -0500

    finished api handler

--- cmd/api/handlers-api.go
+++ cmd/api/handlers-api.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"myapp/internal/cards"
 	"net/http"
+	"strconv"
 )
 
 // the payload we are receiving from the front end
@@ -12,24 +14,71 @@ type stripePayload struct {
 }
 
 // the response we are sending back the front end
+// ,omitempty means if the field is blank, don't show it
 type jsonResponse struct {
 	OK bool `json:"ok"`
-	Message string 	`json:"message"`
-	Content string `json:"content"`
-	ID int `json:"id"`
+	Message string 	`json:"message,omitempty"`
+	Content string `json:"content,omitempty"`
+	ID int `json:"id,omitempty"`
 }
 
 func (app *application) GetPaymentIntent(w http.ResponseWriter, r *http.Request) {
-	// send some manually built json back to the end user
-	j := jsonResponse {
-		OK: true,
-	}
 
-	// convert the response to json and send it back
-	out, err := json.MarshalIndent(j, "", "   ")
+	var payload stripePayload
+	// get the body of the request and decode it into the payload variable
+	err := json.NewDecoder(r.Body).Decode(&payload)
 	if err != nil {
 		app.errorLog.Println(err)
+		return
 	}
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(out)
+
+	// cast amount from string to int
+	amount, err := strconv.Atoi(payload.Amount)
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
+	card := cards.Card {
+		Secret: app.config.stripe.secret,
+		Key: app.config.stripe.key,
+		Currency: payload.Currency,
+	}
+
+	okay := true
+
+	// connects to stripe for us
+	// we get a payment intent back
+	pi, msg, err := card.Charge(payload.Currency, amount)
+	if err != nil {
+		okay = false
+	}
+
+	if okay {
+		// if the charge succeeded and the payment intent comes back with no error, send json back
+		// create the json we want to send back
+		out, err := json.MarshalIndent(pi, "", "   ")
+		if err != nil {
+			app.errorLog.Println(err)
+			return
+		}
+		// set the header and send it out
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(out)
+	} else {
+		// if the charge failed
+		j := jsonResponse {
+			OK: false,
+			Message: msg,
+			Content: "",
+
+		}
+		// convert the response to json and send it back
+		out, err := json.MarshalIndent(j, "", "   ")
+		if err != nil {
+		app.errorLog.Println(err)
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(out)
+	}
 }
