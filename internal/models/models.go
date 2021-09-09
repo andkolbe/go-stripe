c846a6eb07a518e77745861341d833e90b6138ce
Andrew <kolbe1129@gmail.com>
2021-09-09 17:25:32 -0500

    created database models

--- internal/models/models.go
+++ internal/models/models.go
@@ -25,17 +25,57 @@ func NewModels(db *sql.DB) Models {
 
 // the type for all widgets
 type Widget struct {
-	ID             int       `json:"id"`
-	Name           string    `json:"name"`
-	Description    string    `json:"description"`
-	InventoryLevel int       `json:"inventory_level"`
-	Price          int       `json:"price"`
-	CreatedAt      time.Time `json:"-"`
-	UpdatedAt      time.Time `json:"-"`
+	ID             int    `json:"id"`
+	Name           string `json:"name"`
+	Description    string `json:"description"`
+	InventoryLevel int    `json:"inventory_level"`
+	Price          int    `json:"price"`
+	Image          string    `json:"image"`
+}
+
+// the type for all orders
+type Order struct {
+	ID            int `json:"id"`
+	WidgetID      int `json:"widget_id"`
+	TransactionID int `json:"transaction_id"`
+	StatusID      int `json:"status_id"`
+	Quantity      int `json:"quantity"`
+	Amount        int `json:"amount"`
+}
+
+// the type for order statuses
+type Status struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+// the type for transaction statuses
+type TransactionStatus struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+// the type for transactions
+type Transaction struct {
+	ID                  int    `json:"id"`
+	Amount              int    `json:"amount"`
+	Currency            string `json:"currency"`
+	LastFour            string `json:"last_four"`
+	BankReturnCode      string `json:"bank_return_code"`
+	TransactionStatusId string `json:"transaction_status_id"`
+}
+
+// the type for users
+type User struct {
+	ID        int    `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
 }
 
 func (m *DBModel) GetWidget(id int) (Widget, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 3 * time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
 	var widget Widget
