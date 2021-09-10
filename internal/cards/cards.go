10152dd27b7942c35782e07eb4b4ec65050996e8
Andrew <kolbe1129@gmail.com>
2021-09-10 14:04:47 -0500

    create methods to create a stripe customer and subscribe to a plan

--- internal/cards/cards.go
+++ internal/cards/cards.go
@@ -1,9 +1,11 @@
 package cards
 
 import (
-	"github.com/stripe/stripe-go/v72/paymentmethod"
+	"github.com/stripe/stripe-go/v72/sub"
 	"github.com/stripe/stripe-go/v72"
+	"github.com/stripe/stripe-go/v72/customer"
 	"github.com/stripe/stripe-go/v72/paymentintent"
+	"github.com/stripe/stripe-go/v72/paymentmethod"
 )
 
 // define a card type that holds information that's necessary to talk to stripe
@@ -68,6 +70,51 @@ func (c *Card) RetrievePaymentIntent(id string) (*stripe.PaymentIntent, error) {
 		return nil, err
 	}
 	return pi, nil
+}
+
+func (c *Card) SubscribeToPlan(cust *stripe.Customer, plan, email, last4, cardType string) (string, error) {
+	// get the stripe customer id from the customer that is passed into this function
+	stripeCustomerID := cust.ID
+	// what we are going to subscribe this customer to
+	items := []*stripe.SubscriptionItemsParams{
+		{Plan: stripe.String(plan)},
+	}
+
+	params := &stripe.SubscriptionParams{
+		Customer: stripe.String(stripeCustomerID),
+		Items: items,
+	}
+
+	// add metadata to the params that we might need later on
+	params.AddMetadata("last_four", last4)
+	params.AddMetadata("card_type", cardType)
+	params.AddExpand("latest_invoice.payment_intent")
+	subscription, err := sub.New(params)
+	if err != nil {
+		return "", err
+	}
+	return subscription.ID, nil
+}
+
+func (c *Card) CreateCustomer(pm, email string) (*stripe.Customer, string, error) {
+	stripe.Key = c.Secret
+	customerParams := &stripe.CustomerParams{
+		PaymentMethod: stripe.String(pm),
+		Email: stripe.String(email),
+		InvoiceSettings: &stripe.CustomerInvoiceSettingsParams{
+			DefaultPaymentMethod: stripe.String(pm),
+		},
+	}
+
+	cust, err := customer.New(customerParams)
+	if err != nil {
+		msg := ""
+		if stripeErr, ok := err.(*stripe.Error); ok {
+			msg = cardErrorMessage(stripeErr.Code)
+		}
+		return nil, msg, err
+	}
+	return cust, "", nil
 }
 
 // returns human readable versions of card error messages
